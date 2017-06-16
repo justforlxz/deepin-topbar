40dd67ca40c4aa89d8ecd703a44027a9b4c8456e
kirigaya <kirigaya@mkacg.com>
2017-06-16 10:34:30 +0800

    Clear code

--- frame/item/stretchitem.cpp
+++ frame/item/stretchitem.cpp
@@ -1,13 +1,8 @@
 #include "stretchitem.h"
-#include <QLabel>
 
 StretchItem::StretchItem(QWidget *parent) : Item(parent)
 {
     setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    QLabel *label = new QLabel(this);
-    label->setFixedWidth(100);
-    label->setStyleSheet("background: red;");
-    setStyleSheet("background: blue;");
 }
 
 Item::ItemType StretchItem::itemType() const
