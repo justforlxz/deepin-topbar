acc1e9350266152a9b49bcdd490cf1ba2d18ecc4
kirigaya <kirigaya@mkacg.com>
2017-07-17 08:51:41 +0800

    Update systeminfo plugin

--- plugins/systeminfo/systeminfowidget.cpp
+++ plugins/systeminfo/systeminfowidget.cpp
@@ -1,30 +1,76 @@
 #include "systeminfowidget.h"
-#include <QIcon>
 #include <QHBoxLayout>
-#include <QLabel>
 
-SystemInfoWidget::SystemInfoWidget(QWidget *parent) : QLabel(parent)
+SystemInfoWidget::SystemInfoWidget(QWidget *parent) : QWidget(parent)
 {
-      setFixedSize(25, 25);
+    setFixedSize(70, 25);
 
-      setStyleSheet("QLabel {"
-                    "color: black;"
-                    "background: transparent;"
-                    "}"
-                    "QLabel:hover {"
-                    "color: white;"
-                    "background: #1E90FF;"
-                    "}");
+    QLabel *up   = new QLabel("↑  ");
+    QLabel *down = new QLabel("↓  ");
 
-      QLabel *label = new QLabel;
-      label->setFixedSize(22, 22);
-      label->setPixmap(QIcon::fromTheme("view-list-details").pixmap(22, 22));
+    m_rxPackets = new QLabel("100KB/s");
+    m_rxPackets->setFixedHeight(10);
 
-      QHBoxLayout *layout = new QHBoxLayout;
+    m_txPackets = new QLabel("49KB/s");
+    m_txPackets->setFixedHeight(10);
 
-      layout->setMargin(0);
-      layout->setSpacing(0);
-      layout->addWidget(label, 0, Qt::AlignCenter);
+    QHBoxLayout *upLayout = new QHBoxLayout;
+    upLayout->setMargin(0);
+    upLayout->setSpacing(0);
+    upLayout->addWidget(up, 0, Qt::AlignLeft);
+    upLayout->addWidget(m_txPackets, 0, Qt::AlignRight);
 
-      setLayout(layout);
+    QHBoxLayout *downLayout = new QHBoxLayout;
+    downLayout->setMargin(0);
+    downLayout->setSpacing(0);
+    downLayout->addWidget(down, 0, Qt::AlignHCenter | Qt::AlignLeft);
+    downLayout->addWidget(m_rxPackets, 0, Qt::AlignHCenter | Qt::AlignRight);
+
+    QVBoxLayout *layout = new QVBoxLayout;
+
+    layout->setMargin(0);
+    layout->setSpacing(1);
+
+    layout->addLayout(downLayout);
+    layout->addLayout(upLayout);
+
+    setLayout(layout);
+
+//    setStyleSheet("QLabel {"
+//                  "color: black;"
+//                  "background: transparent;"
+//                  "}"
+//                  "QLabel:hover {"
+//                  "color: white;"
+//                  "background: #1E90FF;"
+//                  "}");
+
+    setStyleSheet(":hover {"
+                  "background: #1E90FF;"
+                  "}"
+                  "{"
+                  ""
+                  "}");
+}
+
+void SystemInfoWidget::enterEvent(QEvent *event)
+{
+    QWidget::enterEvent(event);
+
+//    setStyleSheet("background: #1E90FF;"
+//                  "QLabel {"
+//                  "color: white;"
+//                  "}");
+
+
+}
+
+void SystemInfoWidget::leaveEvent(QEvent *event)
+{
+    QWidget::leaveEvent(event);
+
+//    setStyleSheet("background: transparent;"
+//                  "QLabel {"
+//                  "color: black;"
+//                  "}");
 }
--- plugins/systeminfo/systeminfowidget.h
+++ plugins/systeminfo/systeminfowidget.h
@@ -1,13 +1,22 @@
 #ifndef SYSTEMINFOWIDGET_H
 #define SYSTEMINFOWIDGET_H
 
+#include <QWidget>
 #include <QLabel>
 
-class SystemInfoWidget : public QLabel
+class SystemInfoWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit SystemInfoWidget(QWidget *parent = 0);
+
+protected:
+    void enterEvent(QEvent *event) Q_DECL_OVERRIDE;
+    void leaveEvent(QEvent *event) Q_DECL_OVERRIDE;
+
+private:
+    QLabel *m_rxPackets;
+    QLabel *m_txPackets;
 };
 
 #endif // SYSTEMINFOWIDGET_H
