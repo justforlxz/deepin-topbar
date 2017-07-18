ca0fe8584dee6f68ef74a7f60b00e5e2374690ea
kirigaya <kirigaya@mkacg.com>
2017-07-18 11:01:19 +0800

    MediaControl plugin: use fontlabel

--- plugins/mediacontrol/View/mediacontrol.cpp
+++ plugins/mediacontrol/View/mediacontrol.cpp
@@ -1,64 +1,95 @@
 #include "mediacontrol.h"
 #include <QHBoxLayout>
 #include <QEvent>
-#include <QPixmap>
-#include <QDebug>
+#include <QMouseEvent>
 
 namespace Plugin {
-    namespace MediaControl {
-        MediaControl::MediaControl(QWidget *parent) : QFrame(parent) {
-            setWindowFlags(Qt::FramelessWindowHint);
+namespace MediaControl {
+MediaControl::MediaControl(QWidget *parent) : QFrame(parent) {
+    setWindowFlags(Qt::FramelessWindowHint);
 
-            m_lastBtn = new DImageButton(":/img/Icons/play_next_normal.png",
-                                         ":/img/Icons/play_next_hover.png",
-                                         ":/img/Icons/play_next_press.png");
+    m_lastBtn = new FontLabel;
+    m_lastBtn->setIcon(QChar(0xE76C), 18);
+    m_lastBtn->installEventFilter(this);
 
-            m_previousBtn = new DImageButton(":/img/Icons/play_previous_normal.png",
-                                             ":/img/Icons/play_previous_hover.png",
-                                             ":/img/Icons/play_previous_press.png");
-            m_pauseBtn = new DImageButton;
+    m_previousBtn = new FontLabel;
+    m_previousBtn->setIcon(QChar(0xE76B), 18);
+    m_previousBtn->installEventFilter(this);
 
-            QHBoxLayout *layout = new QHBoxLayout;
-            layout->setMargin(2);
-            layout->setSpacing(0);
+    m_pauseBtn = new FontLabel;
+    m_pauseBtn->installEventFilter(this);
 
-            layout->addWidget(m_previousBtn, 0, Qt::AlignLeft | Qt::AlignVCenter);
-            layout->addWidget(m_pauseBtn, 0, Qt::AlignCenter | Qt::AlignVCenter);
-            layout->addWidget(m_lastBtn, 0, Qt::AlignRight | Qt::AlignVCenter);
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->setMargin(2);
+    layout->setSpacing(0);
 
-            setLayout(layout);
+    layout->addWidget(m_previousBtn, 0, Qt::AlignLeft | Qt::AlignVCenter);
+    layout->addWidget(m_pauseBtn, 0, Qt::AlignCenter | Qt::AlignVCenter);
+    layout->addWidget(m_lastBtn, 0, Qt::AlignRight | Qt::AlignVCenter);
 
-            connect(m_previousBtn, &DImageButton::clicked, this, &MediaControl::requestPrevious);
-            connect(m_pauseBtn, &DImageButton::clicked, this, &MediaControl::requestPause);
-            connect(m_lastBtn, &DImageButton::clicked, this, &MediaControl::requestLast);
-        }
+    setLayout(layout);
+}
 
-        void MediaControl::setPlayState(MediaControl::PlayState state) {
-            m_playState = state;
+void MediaControl::setPlayState(MediaControl::PlayState state) {
+    m_playState = state;
 
-            switch (state) {
-            case Play: {
-                m_pauseBtn->setNormalPic(":/img/Icons/play_pause_normal.png");
-                m_pauseBtn->setHoverPic(":/img/Icons/play_pause_hover.png");
-                m_pauseBtn->setPressPic(":/img/Icons/play_pause_press.png");
-                break;
-            }
-            case Stop:
-                break;
-            case Pause: {
-                m_pauseBtn->setNormalPic(":/img/Icons/play_start_normal.png");
-                m_pauseBtn->setHoverPic(":/img/Icons/play_start_hover.png");
-                m_pauseBtn->setPressPic(":/img/Icons/play_start_press.png");
-                break;
-            }
-            case Last:
-                break;
-            case Previous:
-                break;
-            default:
-                break;
-            }
-        }
-
+    switch (state) {
+    case Play: {
+        m_pauseBtn->setIcon(QChar(0xE769), 18);
+        break;
     }
+    case Stop:
+        break;
+    case Pause: {
+        m_pauseBtn->setIcon(QChar(0xE768), 18);
+        break;
+    }
+    case Last:
+        break;
+    case Previous:
+        break;
+    default:
+        break;
+    }
+}
+
+bool MediaControl::eventFilter(QObject *watched, QEvent *event)
+{
+    if (watched == m_lastBtn) {
+        if (event->type() == QMouseEvent::Enter)
+            m_lastBtn->setStyleSheet("color: #00BFFF");
+        if (event->type() == QMouseEvent::Leave)
+            m_lastBtn->setStyleSheet("color: black");
+        if (event->type() == QMouseEvent::MouseButtonPress)
+            m_lastBtn->setStyleSheet("color: #1E90FF");
+        if (event->type() == QMouseEvent::MouseButtonRelease)
+            emit requestLast();
+    }
+
+    if (watched == m_previousBtn) {
+        if (event->type() == QMouseEvent::Enter)
+            m_previousBtn->setStyleSheet("color: #00BFFF");
+        if (event->type() == QMouseEvent::Leave)
+            m_previousBtn->setStyleSheet("color: black");
+        if (event->type() == QMouseEvent::MouseButtonPress)
+            m_previousBtn->setStyleSheet("color: #1E90FF");
+        if (event->type() == QMouseEvent::MouseButtonRelease)
+            emit requestPrevious();
+    }
+
+    if (watched == m_pauseBtn) {
+        if (event->type() == QMouseEvent::Enter)
+            m_pauseBtn->setStyleSheet("color: #00BFFF");
+        if (event->type() == QMouseEvent::Leave)
+            m_pauseBtn->setStyleSheet("color: black");
+        if (event->type() == QMouseEvent::MouseButtonPress)
+            m_pauseBtn->setStyleSheet("color: #1E90FF");
+        if (event->type() == QMouseEvent::MouseButtonRelease)
+            emit requestPause();
+    }
+
+    return false;
+}
+
+}
 }
--- plugins/mediacontrol/View/mediacontrol.h
+++ plugins/mediacontrol/View/mediacontrol.h
@@ -4,39 +4,43 @@
 #include <QFrame>
 #include <QLabel>
 #include <dimagebutton.h>
+#include "fontlabel.h"
 
 DWIDGET_USE_NAMESPACE
 
 namespace Plugin {
     namespace MediaControl {
-        class MediaControl : public QFrame
-        {
-            Q_OBJECT
-        public:
-            explicit MediaControl(QWidget *parent = 0);
+    class MediaControl : public QFrame
+    {
+        Q_OBJECT
+    public:
+        explicit MediaControl(QWidget *parent = 0);
 
-            enum PlayState {
-                Play,
-                Pause,
-                Stop,
-                Last,
-                Previous
-            };
-
-        signals:
-            void requestPrevious();
-            void requestLast();
-            void requestPause();
-
-        public slots:
-            void setPlayState(PlayState state);
-
-        private:
-            DImageButton *m_lastBtn;
-            DImageButton *m_previousBtn;
-            DImageButton *m_pauseBtn;
-            PlayState m_playState;
+        enum PlayState {
+            Play,
+            Pause,
+            Stop,
+            Last,
+            Previous
         };
+
+    signals:
+        void requestPrevious();
+        void requestLast();
+        void requestPause();
+
+    public slots:
+        void setPlayState(PlayState state);
+
+    protected:
+        bool eventFilter(QObject *watched, QEvent *event) Q_DECL_OVERRIDE;
+
+    private:
+        FontLabel *m_lastBtn;
+        FontLabel *m_previousBtn;
+        FontLabel *m_pauseBtn;
+        PlayState m_playState;
+    };
     }
 }
 
