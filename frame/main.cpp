dd02c0778373af57855772780be74a4b46b12424
kirigaya <kirigaya@mkacg.com>
2017-08-25 16:22:37 +0800

    Regression function to Frame

--- frame/main.cpp
+++ frame/main.cpp
@@ -1,37 +1,7 @@
 #include "mainframe.h"
 #include <DApplication>
-#include <xcb/xcb.h>
-#include <xcb/xcb_ewmh.h>
-#include <QtX11Extras/QX11Info>
 
 DWIDGET_USE_NAMESPACE
-
-void register_wm_state(WId winid) {
-    xcb_ewmh_connection_t m_ewmh_connection;
-    xcb_intern_atom_cookie_t *cookie = xcb_ewmh_init_atoms(QX11Info::connection(), &m_ewmh_connection);
-    xcb_ewmh_init_atoms_replies(&m_ewmh_connection, cookie, NULL);
-
-    xcb_atom_t atoms[2];
-    atoms[0] = m_ewmh_connection._NET_WM_WINDOW_TYPE_DOCK;
-    atoms[1] = m_ewmh_connection._NET_WM_STATE_BELOW;
-    xcb_ewmh_set_wm_window_type(&m_ewmh_connection, winid, 1, atoms);
-
-    xcb_ewmh_wm_strut_partial_t strutPartial;
-    memset(&strutPartial, 0, sizeof(xcb_ewmh_wm_strut_partial_t));
-
-    // clear strut partial
-    xcb_ewmh_set_wm_strut_partial(&m_ewmh_connection, winid, strutPartial);
-
-    // set strct partial
-    xcb_ewmh_wm_strut_partial_t strut_partial;
-    memset(&strut_partial, 0, sizeof(xcb_ewmh_wm_strut_partial_t));
-
-    strut_partial.top = 28;
-    strut_partial.top_start_x = 0;
-    strut_partial.top_end_x = 1920;
-
-    xcb_ewmh_set_wm_strut_partial(&m_ewmh_connection, winid, strut_partial);
-}
 
 int main(int argc, char *argv[])
 {
@@ -51,7 +21,6 @@ int main(int argc, char *argv[])
         a.setFont(font);
 
         MainFrame *mainFrame = new MainFrame;
-        register_wm_state(mainFrame->winId());
         mainFrame->show();
 
         return a.exec();
--- frame/mainframe.cpp
+++ frame/mainframe.cpp
@@ -3,6 +3,9 @@
 #include <QScreen>
 #include <QApplication>
 #include <QRect>
+#include <xcb/xcb.h>
+#include <xcb/xcb_ewmh.h>
+#include <QtX11Extras/QX11Info>
 
 DWIDGET_USE_NAMESPACE
 
@@ -64,4 +67,29 @@ void MainFrame::screenChanged()
     move(screen.x(), 0);
     m_mainPanel->move(0, 0);
     m_blurEffectWidget->move(0, 0);
+
+    xcb_ewmh_connection_t m_ewmh_connection;
+    xcb_intern_atom_cookie_t *cookie = xcb_ewmh_init_atoms(QX11Info::connection(), &m_ewmh_connection);
+    xcb_ewmh_init_atoms_replies(&m_ewmh_connection, cookie, NULL);
+
+    xcb_atom_t atoms[2];
+    atoms[0] = m_ewmh_connection._NET_WM_WINDOW_TYPE_DOCK;
+    atoms[1] = m_ewmh_connection._NET_WM_STATE_BELOW;
+    xcb_ewmh_set_wm_window_type(&m_ewmh_connection, winId(), 1, atoms);
+
+    xcb_ewmh_wm_strut_partial_t strutPartial;
+    memset(&strutPartial, 0, sizeof(xcb_ewmh_wm_strut_partial_t));
+
+    // clear strut partial
+    xcb_ewmh_set_wm_strut_partial(&m_ewmh_connection, winId(), strutPartial);
+
+    // set strct partial
+    xcb_ewmh_wm_strut_partial_t strut_partial;
+    memset(&strut_partial, 0, sizeof(xcb_ewmh_wm_strut_partial_t));
+
+    strut_partial.top = TOPHEIGHT;
+    strut_partial.top_start_x = x();
+    strut_partial.top_end_x = width();
+
+    xcb_ewmh_set_wm_strut_partial(&m_ewmh_connection, winId(), strut_partial);
 }
