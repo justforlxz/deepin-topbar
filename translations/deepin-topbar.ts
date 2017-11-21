d16cbd2d39f1cfd0e42d54d8f884e23a81c8d1fb
kirigaya <kirigaya@mkacg.com>
2017-11-21 14:23:30 +0800

    update ts

--- translations/deepin-topbar.ts
+++ translations/deepin-topbar.ts
@@ -113,6 +113,7 @@
         <source>%n day(s) ago</source>
         <translation type="unfinished">
             <numerusform></numerusform>
+            <numerusform></numerusform>
         </translation>
     </message>
 </context>
