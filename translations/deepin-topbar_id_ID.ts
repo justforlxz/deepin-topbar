acbeb1ee130f097e6a128ef94406566772f46a96
Rafi Priatna K <stormrider136@GMAIL.COM>
2018-03-30 23:12:00 +0700

    deepin-topbar_id_ID.ts
    Translation to Indonesia

---
+++ translations/deepin-topbar_id_ID.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="id_ID" version="2.1">
+<context>
+    <name>dtb::datetime::DateTimePlugin</name>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="49"/>
+        <source>24 Hour Time</source>
+        <translation>24 Jam</translation>
+    </message>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="53"/>
+        <source>12 Hour Time</source>
+        <translation>12 Jam</translation>
+    </message>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="69"/>
+        <source>Time Format</source>
+        <translation>Format Waktu</translation>
+    </message>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="74"/>
+        <source>Time Settings</source>
+        <translation>Pengaturan</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::indicator::AppstoreAction</name>
+    <message numerus="yes">
+        <location filename="../modules/indicator/View/appstoreaction.cpp" line="108"/>
+        <source>%n update(s)</source>
+        <translation><numerusform>%n Pembaruan</numerusform></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::indicator::PositionWidget</name>
+    <message>
+        <location filename="../modules/indicator/View/positionwidget.cpp" line="35"/>
+        <source>Position</source>
+        <translation>Posisi</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::indicator::SystemLogo</name>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="67"/>
+        <source>About</source>
+        <translation>Tentang</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="79"/>
+        <source>Preference setting</source>
+        <translation>Pengaturan Preferensi</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="82"/>
+        <source>Force quit</source>
+        <translation>Paksa keluar</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="83"/>
+        <source>Sleep</source>
+        <translation>Sleep</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="84"/>
+        <source>Restart</source>
+        <translation>Muat ulang</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="85"/>
+        <source>Power off</source>
+        <translation>Matikan</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="86"/>
+        <source>Logout for %1</source>
+        <translation>Keluar untuk %1</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::network::RefreshAction</name>
+    <message>
+        <location filename="../modules/network/item/applet/refreshaction.cpp" line="16"/>
+        <source>List state: %1</source>
+        <translation>Keadaan：%1</translation>
+    </message>
+    <message>
+        <location filename="../modules/network/item/applet/refreshaction.cpp" line="16"/>
+        <source>Refreshing...</source>
+        <translation>Menyegarkan...</translation>
+    </message>
+    <message>
+        <location filename="../modules/network/item/applet/refreshaction.cpp" line="16"/>
+        <source>No refresh</source>
+        <translation>Tanpa penyegaran</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::network::WiredItem</name>
+    <message>
+        <location filename="../modules/network/item/wireditem.cpp" line="27"/>
+        <source>Open Network Preferences...</source>
+        <translation>Pengaturan jaringan</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::network::WirelessItem</name>
+    <message>
+        <location filename="../modules/network/item/wirelessitem.cpp" line="89"/>
+        <source>Join Other Network...</source>
+        <translation>Masuk ke jaringan lain</translation>
+    </message>
+    <message>
+        <location filename="../modules/network/item/wirelessitem.cpp" line="90"/>
+        <source>Open Network Preferences...</source>
+        <translation>Pengaturan jaringan</translation>
+    </message>
+    <message>
+        <location filename="../modules/network/item/wirelessitem.cpp" line="348"/>
+        <source>Password required to connect to &lt;font color=&quot;#faca57&quot;&gt;%1&lt;/font&gt;</source>
+        <translation>Dibutuhkan sandi untuk menghubungkan ke &lt;font color=&quot;#faca57&quot;&gt;%1&lt;/font&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::notify::NotifyModel</name>
+    <message>
+        <location filename="../modules/notify/notifymodel.cpp" line="180"/>
+        <source>Yesterday</source>
+        <translation>Kemarin</translation>
+    </message>
+    <message>
+        <location filename="../modules/notify/notifymodel.cpp" line="183"/>
+        <source>The day before yesterday</source>
+        <translation>Kemarin lusa</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../modules/notify/notifymodel.cpp" line="189"/>
+        <source>%n day(s) ago</source>
+        <translation><numerusform>%n hari yang lalu</numerusform></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::power::PowerWidget</name>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="49"/>
+        <source>Power source:</source>
+        <translation>Sumber daya：</translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="49"/>
+        <source>Battery</source>
+        <translation>Baterai</translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="49"/>
+        <source>Direct current</source>
+        <translation>Arus searah</translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="135"/>
+        <source>Show percentage</source>
+        <translation>Perlihatkan persentase</translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="136"/>
+        <source>Open Energy saver preferences</source>
+        <translation>Buka penghemat daya</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::sound::SoundApplet</name>
+    <message>
+        <location filename="../modules/sound/soundapplet.cpp" line="41"/>
+        <source>Device</source>
+        <translation>Perangkat</translation>
+    </message>
+    <message>
+        <location filename="../modules/sound/soundapplet.cpp" line="61"/>
+        <source>Application</source>
+        <translation>Aplikasi</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::sound::SoundItem</name>
+    <message>
+        <location filename="../modules/sound/sounditem.cpp" line="56"/>
+        <source>Advanced Settings</source>
+        <translation>Pengaturan lanjutan</translation>
+    </message>
+</context>
+</TS>
