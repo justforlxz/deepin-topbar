eb0e3c19bfe0ccda704ad56877aa7c3d3e92c4bd
Alvaro Samudio <alvarosamudio1@gmail.com>
2018-04-22 02:03:50 +0000

    Spanish translations
    Spanish translations of the topbar

---
+++ translations/deepin-topbar_es_ES.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es_ES" version="1.0">
+<context>
+    <name>dtb::datetime::DateTimePlugin</name>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="49"/>
+        <source>24 Hour Time</source>
+        <translation>Horarios de 24 horas</translation>
+    </message>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="53"/>
+        <source>12 Hour Time</source>
+        <translation>Horarios de 12 horas</translation>
+    </message>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="69"/>
+        <source>Time Format</source>
+        <translation>Formato de Hora</translation>
+    </message>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="74"/>
+        <source>Time Settings</source>
+        <translation>Ajustes de hora</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::indicator::AppstoreAction</name>
+    <message numerus="yes">
+        <location filename="../modules/indicator/View/appstoreaction.cpp" line="108"/>
+        <source>%n update(s)</source>
+        <translation><numerusform>%nactualización(es)</numerusform></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::indicator::PositionWidget</name>
+    <message>
+        <location filename="../modules/indicator/View/positionwidget.cpp" line="35"/>
+        <source>Position</source>
+        <translation>Posición</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::indicator::SystemLogo</name>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="67"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="79"/>
+        <source>Preference setting</source>
+        <translation>Configuración de preferencias</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="82"/>
+        <source>Force quit</source>
+        <translation>Forzar la salida</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="83"/>
+        <source>Sleep</source>
+        <translation>Suspender</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="84"/>
+        <source>Restart</source>
+        <translation>Reiniciar</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="85"/>
+        <source>Power off</source>
+        <translation>Apagar</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="86"/>
+        <source>Logout for %1</source>
+        <translation>Salir de %1</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::network::RefreshAction</name>
+    <message>
+        <location filename="../modules/network/item/applet/refreshaction.cpp" line="16"/>
+        <source>List state: %1</source>
+        <translation>Estado de lista: %1</translation>
+    </message>
+    <message>
+        <location filename="../modules/network/item/applet/refreshaction.cpp" line="16"/>
+        <source>Refreshing...</source>
+        <translation>Refrescar...</translation>
+    </message>
+    <message>
+        <location filename="../modules/network/item/applet/refreshaction.cpp" line="16"/>
+        <source>No refresh</source>
+        <translation>Sin Refrescar</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::network::WiredItem</name>
+    <message>
+        <location filename="../modules/network/item/wireditem.cpp" line="27"/>
+        <source>Open Network Preferences...</source>
+        <translation>Abrir preferencias de red...</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::network::WirelessItem</name>
+    <message>
+        <location filename="../modules/network/item/wirelessitem.cpp" line="89"/>
+        <source>Join Other Network...</source>
+        <translation>Unirse a otra red...</translation>
+    </message>
+    <message>
+        <location filename="../modules/network/item/wirelessitem.cpp" line="90"/>
+        <source>Open Network Preferences...</source>
+        <translation>Abrir preferencias de red...</translation>
+    </message>
+    <message>
+        <location filename="../modules/network/item/wirelessitem.cpp" line="348"/>
+        <source>Password required to connect to &lt;font color=&quot;#faca57&quot;&gt;%1&lt;/font&gt;</source>
+        <translation>Requiere contraseña para conectarse &lt;font color=&quot;#faca57&quot;&gt;%1&lt;/font&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::notify::NotifyModel</name>
+    <message>
+        <location filename="../modules/notify/notifymodel.cpp" line="180"/>
+        <source>Yesterday</source>
+        <translation>Ayer</translation>
+    </message>
+    <message>
+        <location filename="../modules/notify/notifymodel.cpp" line="183"/>
+        <source>The day before yesterday</source>
+        <translation>Antes de ayer</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../modules/notify/notifymodel.cpp" line="189"/>
+        <source>%n day(s) ago</source>
+        <translation><numerusform>%ndía(s) atrás</numerusform></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::power::PowerWidget</name>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="49"/>
+        <source>Power source:</source>
+        <translation>Fuente de alimentación:</translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="49"/>
+        <source>Battery</source>
+        <translation>Batería</translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="49"/>
+        <source>Direct current</source>
+        <translation>Corriente continua</translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="135"/>
+        <source>Show percentage</source>
+        <translation>Mostrar porcentaje</translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="136"/>
+        <source>Open Energy saver preferences</source>
+        <translation>Abrit preferencias de ahorro de energía</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::sound::SoundApplet</name>
+    <message>
+        <location filename="../modules/sound/soundapplet.cpp" line="41"/>
+        <source>Device</source>
+        <translation>Dispositivo</translation>
+    </message>
+    <message>
+        <location filename="../modules/sound/soundapplet.cpp" line="61"/>
+        <source>Application</source>
+        <translation>Aplicación</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::sound::SoundItem</name>
+    <message>
+        <location filename="../modules/sound/sounditem.cpp" line="56"/>
+        <source>Advanced Settings</source>
+        <translation>Ajustes avanzados</translation>
+    </message>
+</context>
+</TS>
