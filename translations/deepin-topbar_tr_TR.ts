3b86f7f04706442194ad010b84fdcabc3f8e2fe9
Caglar Turali <caglarturali@gmail.com>
2018-10-01 03:38:33 +0300

    Add Turkish translation

---
+++ translations/deepin-topbar_tr_TR.ts
@@ -0,0 +1,194 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="tr_TR" version="1.0">
+<context>
+    <name>dtb::datetime::DateTimePlugin</name>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="49"/>
+        <source>24 Hour Time</source>
+        <translation>24 Saat Biçimi</translation>
+    </message>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="53"/>
+        <source>12 Hour Time</source>
+        <translation>12 Saat Biçimi</translation>
+    </message>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="69"/>
+        <source>Time Format</source>
+        <translation>Saat Formatı</translation>
+    </message>
+    <message>
+        <location filename="../modules/timewidget/datetimeplugin.cpp" line="74"/>
+        <source>Time Settings</source>
+        <translation>Saat ve Tarih Ayarları</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::indicator::AppstoreAction</name>
+    <message numerus="yes">
+        <location filename="../modules/indicator/View/storeaction.cpp" line="108"/>
+        <source>%n update(s)</source>
+        <translation>
+            <numerusform>%n Güncelleme(ler)</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>dtb::indicator::PositionWidget</name>
+    <message>
+        <location filename="../modules/indicator/View/positionwidget.cpp" line="35"/>
+        <source>Position</source>
+        <translation>Pozisyon</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::indicator::SystemLogo</name>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="67"/>
+        <source>About</source>
+        <translation>Hakkında</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="79"/>
+        <source>Preference setting</source>
+        <translation>Tercihler</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="82"/>
+        <source>Force quit</source>
+        <translation>Çıkmaya Zorla</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="83"/>
+        <source>Sleep</source>
+        <translation>Uyut</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="84"/>
+        <source>Restart</source>
+        <translation>Yeniden Başlat</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="85"/>
+        <source>Power off</source>
+        <translation>Kapat</translation>
+    </message>
+    <message>
+        <location filename="../modules/indicator/View/systemlogo.cpp" line="86"/>
+        <source>Logout for %1</source>
+        <translation>%1 Oturumunu Kapat</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::network::RefreshAction</name>
+    <message>
+        <location filename="../modules/network/item/applet/refreshaction.cpp" line="16"/>
+        <source>List state: %1</source>
+        <translation>Listele: %1</translation>
+    </message>
+    <message>
+        <location filename="../modules/network/item/applet/refreshaction.cpp" line="16"/>
+        <source>Refreshing...</source>
+        <translation>Yenileniyor...</translation>
+    </message>
+    <message>
+        <location filename="../modules/network/item/applet/refreshaction.cpp" line="16"/>
+        <source>No refresh</source>
+        <translation>Yenilenmedi</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::network::WiredItem</name>
+    <message>
+        <location filename="../modules/network/item/wireditem.cpp" line="27"/>
+        <source>Open Network Preferences...</source>
+        <translation>Ağ Tercihlerini Aç...</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::network::WirelessItem</name>
+    <message>
+        <location filename="../modules/network/item/wirelessitem.cpp" line="89"/>
+        <source>Join Other Network...</source>
+        <translation>Başka Ağa Bağlan...</translation>
+    </message>
+    <message>
+        <location filename="../modules/network/item/wirelessitem.cpp" line="90"/>
+        <source>Open Network Preferences...</source>
+        <translation>Ağ Tercihlerini Aç...</translation>
+    </message>
+    <message>
+        <location filename="../modules/network/item/wirelessitem.cpp" line="348"/>
+        <source>Password required to connect to &lt;font color=&quot;#faca57&quot;&gt;%1&lt;/font&gt;</source>
+        <translation>&lt;font color=&quot;#faca57&quot;&gt;%1&lt;/font&gt; ağına bağlanmak için parola gerekli</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::notify::NotifyModel</name>
+    <message>
+        <location filename="../modules/notify/notifymodel.cpp" line="180"/>
+        <source>Yesterday</source>
+        <translation>Dün</translation>
+    </message>
+    <message>
+        <location filename="../modules/notify/notifymodel.cpp" line="183"/>
+        <source>The day before yesterday</source>
+        <translation>Önceki Gün</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../modules/notify/notifymodel.cpp" line="189"/>
+        <source>%n day(s) ago</source>
+        <translation>
+            <numerusform>%n Gün Önce</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>dtb::power::PowerWidget</name>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="49"/>
+        <source>Power source:</source>
+        <translation>Güç Kaynağı:</translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="49"/>
+        <source>Battery</source>
+        <translation>Pil</translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="49"/>
+        <source>Direct current</source>
+        <translation>Şebekeye Bağlı</translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="135"/>
+        <source>Show percentage</source>
+        <translation>Yüzde Olarak Göster</translation>
+    </message>
+    <message>
+        <location filename="../modules/power/powerwidget.cpp" line="136"/>
+        <source>Open Energy saver preferences</source>
+        <translation>Güç Yönetimi Seçeneklerini Aç</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::sound::SoundApplet</name>
+    <message>
+        <location filename="../modules/sound/soundapplet.cpp" line="41"/>
+        <source>Device</source>
+        <translation>Cihaz</translation>
+    </message>
+    <message>
+        <location filename="../modules/sound/soundapplet.cpp" line="61"/>
+        <source>Application</source>
+        <translation>Uygulama</translation>
+    </message>
+</context>
+<context>
+    <name>dtb::sound::SoundItem</name>
+    <message>
+        <location filename="../modules/sound/sounditem.cpp" line="56"/>
+        <source>Advanced Settings</source>
+        <translation>Gelişmiş Seçenekler</translation>
+    </message>
+</context>
+</TS>
