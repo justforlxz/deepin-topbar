38f6d74327c144a905b6ffd666b2984951978bcd
justforlxz <justforlxz@gmail.com>
2018-12-12 17:20:09 +0800

    add us ts file

---
+++ translations/deepin-topbar_en_US.ts
@@ -0,0 +1,287 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>NotifyDelegate</name>
+    <message>
+        <location filename="../src/modules/notify/notifydelegate.cpp" line="208"/>
+        <source>Yesterday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/notify/notifydelegate.cpp" line="211"/>
+        <source>The day before yesterday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/modules/notify/notifydelegate.cpp" line="217"/>
+        <source>%n day(s) ago</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>NotifyWidget</name>
+    <message>
+        <location filename="../src/modules/notify/notifywidget.cpp" line="33"/>
+        <source>No system notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/notify/notifywidget.cpp" line="36"/>
+        <source>Clear all</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PositionWidget</name>
+    <message>
+        <location filename="../src/modules/indicator/View/positionwidget.cpp" line="29"/>
+        <source>Home</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/indicator/View/positionwidget.cpp" line="30"/>
+        <source>Desktop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/indicator/View/positionwidget.cpp" line="31"/>
+        <source>Document</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/indicator/View/positionwidget.cpp" line="32"/>
+        <source>Photo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/indicator/View/positionwidget.cpp" line="33"/>
+        <source>Video</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::account::AccountWidget</name>
+    <message>
+        <location filename="../src/modules/account/accountwidget.cpp" line="98"/>
+        <source>Lock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/account/accountwidget.cpp" line="99"/>
+        <source>Logout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/account/accountwidget.cpp" line="100"/>
+        <source>Suspend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/account/accountwidget.cpp" line="101"/>
+        <source>Shutdown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/account/accountwidget.cpp" line="102"/>
+        <source>Restart</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::datetime::DateTimePlugin</name>
+    <message>
+        <location filename="../src/modules/timewidget/datetimeplugin.cpp" line="33"/>
+        <source>24 Hour Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/timewidget/datetimeplugin.cpp" line="38"/>
+        <source>Time Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::indicator::AppstoreAction</name>
+    <message numerus="yes">
+        <location filename="../src/modules/indicator/View/appstoreaction.cpp" line="108"/>
+        <source>%n update(s)</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>dtb::indicator::PositionWidget</name>
+    <message>
+        <location filename="../src/modules/indicator/View/positionwidget.cpp" line="46"/>
+        <source>Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::indicator::SystemLogo</name>
+    <message>
+        <location filename="../src/modules/indicator/View/systemlogo.cpp" line="61"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/indicator/View/systemlogo.cpp" line="73"/>
+        <source>Preference setting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/indicator/View/systemlogo.cpp" line="79"/>
+        <source>Force quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/indicator/View/systemlogo.cpp" line="80"/>
+        <source>Sleep</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/indicator/View/systemlogo.cpp" line="81"/>
+        <source>Restart</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/indicator/View/systemlogo.cpp" line="82"/>
+        <source>Power off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/indicator/View/systemlogo.cpp" line="83"/>
+        <source>Logout for %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::network::RefreshAction</name>
+    <message>
+        <location filename="../src/modules/network/item/applet/refreshaction.cpp" line="16"/>
+        <source>List state: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/network/item/applet/refreshaction.cpp" line="16"/>
+        <source>Refreshing...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/network/item/applet/refreshaction.cpp" line="16"/>
+        <source>No refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::power::PowerWidget</name>
+    <message>
+        <location filename="../src/modules/power/powerwidget.cpp" line="54"/>
+        <source>Power source:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/power/powerwidget.cpp" line="54"/>
+        <source>Battery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/power/powerwidget.cpp" line="54"/>
+        <source>Direct current</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/power/powerwidget.cpp" line="111"/>
+        <source>Show percentage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/power/powerwidget.cpp" line="112"/>
+        <source>Open Energy saver preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::sound::SoundApplet</name>
+    <message>
+        <location filename="../src/modules/sound/soundapplet.cpp" line="41"/>
+        <source>Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/sound/soundapplet.cpp" line="61"/>
+        <source>Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::sound::SoundItem</name>
+    <message>
+        <location filename="../src/modules/sound/sounditem.cpp" line="77"/>
+        <source>Advanced Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::wallpaper::WallpaperSettings</name>
+    <message>
+        <location filename="../src/modules/wallpaper/wallpapersettings.cpp" line="39"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/wallpaper/wallpapersettings.cpp" line="40"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/wallpaper/wallpapersettings.cpp" line="41"/>
+        <source>Clean</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/wallpaper/wallpapersettings.cpp" line="42"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/wallpaper/wallpapersettings.cpp" line="67"/>
+        <source>Add Path</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dtb::wallpaper::WallpaperWidget</name>
+    <message>
+        <location filename="../src/modules/wallpaper/wallpaperwidget.cpp" line="70"/>
+        <source>DreamScene</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/wallpaper/wallpaperwidget.cpp" line="79"/>
+        <source>Skip to next wallpaper</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/wallpaper/wallpaperwidget.cpp" line="80"/>
+        <source>Browsing mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/wallpaper/wallpaperwidget.cpp" line="81"/>
+        <source>About DreamSense</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/wallpaper/wallpaperwidget.cpp" line="82"/>
+        <source>Preferences...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
